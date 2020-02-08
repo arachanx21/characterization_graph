8c5c67f4b740dd26a905bbd4d9a46cba22cd9147
Ahmad Chandra <chandramaulana31@hotmail.com>
2020-02-08 22:13:17 +0700

    Add files via upload

---
+++ txt_converter_2.py
@@ -0,0 +1,28 @@
+def from_txt_to_csv(sourcefile,desired_filename):
+    import csv
+    import pandas as pd
+    file=open(sourcefile,'r')
+    raw_text=file.readlines()
+    #x,y=raw_text[0].split(' ')
+    #raw_text.close()
+    for i in raw_text:
+        text=i.split(' ')
+        x=text[0]
+        y=text[1]
+        with open(desired_filename,"a") as f:
+            writer = csv.writer(f,delimiter=",")
+            writer.writerow([x,y])
+        print('adding: ',x,',',y,sep='')
+    print(pd.read_csv(desired_filename))
+    print('file',desired_filename,'created')
+    print(pd.read_csv(desired_filename))
+    print('file',desired_filename,'created')
+    file.close()
+    #clearing the shit
+    data=pd.read_csv(desired_filename)
+    for i in 
+    
+
+#sourcefile=input("enter source file name: ")
+#desired_filename=input("enter desired source file name: ")
+#from_txt_to_csv(sourcefile,desired_filename)
